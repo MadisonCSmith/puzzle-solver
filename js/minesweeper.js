188f3034fc97bf8075d46429ce89953abd93b1d5
Madison Smith <mady.c.smith@gmail.com>
2020-06-13 21:35:04 +1200

    refactored into createNewCell function, builds grid after load- not hardcoded, got convertToCleared function mostly working- inputs are still weird, got colors for unknown/cleared cells okay including dark/light colors, fixed bug with cell ids

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -3,22 +3,30 @@
 // update game each time puzzle updated - adding and removing stuff from puzzle
 // user just needs to input numbers and clear spaces - game inputs bombs and more clear spaces
 // double check that values in resize grid form have to be numbers, resonable numbers- not 100000000, and can reasonably fit on screen
+// replace red color with bombs
+// do click modes - 
+// do color crisscrossed patterns like in other games
+// have input field for number of bombs if present in other games
+// do fancy css stuff so it actually looks good
+// center input fields in resize grid form
+// switch height and width on resize form?
+// make puzzle always square 
+// make sure only enter numbers 1-6 in cell inputs
 
 
-// called when submitted
-document.getElementById('grid-size-form').addEventListener('submit', resizeGrid);
-
+// calls resize function when resize form submitted
+document.getElementById('grid-size-form').addEventListener('submit', function() { 
+    event.preventDefault(); // prevent page from refreshing when submitted
+    resizeGrid();
+});
 
 // resets and resizes grid 
-function resizeGrid() {
-    event.preventDefault(); // prevent page from refreshing when submitted
+function resizeGrid(width, height) {
     resetGrid();
 
     var width = document.getElementById("grid-width").value;
     var height = document.getElementById("grid-height").value; 
     var grid = document.getElementById("grid");
-
-    console.log(grid.rows.length);
 
     // resizes number of columns
     for (var i = 0; i < grid.rows.length; i++) {
@@ -28,8 +36,7 @@ function resizeGrid() {
         while (grid.rows[i].cells.length < width) {
             var row = document.getElementById(rowID);
             var cell = row.insertCell(grid.rows[i].cells.length);
-            cell.innerHTML = "<input value='' id='width' type='text' class='validate'>";
-            cell.setAttribute('id', 'col-' + grid.rows[i].cells.length);
+            createNewCell(cell, i + '-' + (grid.rows[i].cells.length - 1));
         }
 
         // if number of columns is more than width value, delete column
@@ -38,16 +45,17 @@ function resizeGrid() {
         }
     }
 
-    // if number of rows is less than height value
+    // if number of rows is less than height value, add row
     while (grid.rows.length < height) {
         var row = grid.insertRow(grid.rows.length);
         row.setAttribute('id', 'row-' + (grid.rows.length - 1));
 
-        // insert same number of cells as first row
+        // insert same number of cells as first row into new row
+        var cellColIndex = 0;
         while (grid.rows[0].cells.length > grid.rows[grid.rows.length - 1].cells.length) {
             var cell = row.insertCell(grid.rows[grid.rows.length - 1].cells.length);
-            cell.innerHTML = "<input value='' id='width' type='text' class='validate'>";
-            cell.setAttribute('id', 'col-' + grid.rows[i].cells.length);
+            createNewCell(cell, grid.rows.length - 1 + '-' + cellColIndex);
+            cellColIndex++;
         }
     }
 
@@ -57,7 +65,36 @@ function resizeGrid() {
     }
 } 
 
+function createNewCell(cell, id) {
+    cell.setAttribute('class', "unknown");
+    cell.setAttribute('id', id);
+    cell.setAttribute('onClick', "convertToCleared(this.id)");
+    if ((parseInt(id.substring(0, 2)) + parseInt(id.substring(id.length - 2, id.length))) % 2 == 0) {
+        cell.className += " light";
+    } else {
+        cell.className += " dark";
+    }
+}
+
 // resets values in grid 
 function resetGrid() {
 
 }
+
+// have undo button just in case of a mistake
+
+// converts cell to cleared if clicked
+function convertToCleared(id) {
+    event.preventDefault();
+    console.log("testing;;;");
+    var cell = document.getElementById(id);
+    cell.classList.remove('unknown');
+    cell.classList.add('cleared');
+    cell.innerHTML = "<input type='number'>";
+
+}
+
+
+
+resizeGrid(); // calls function straight away to create grid - grid is initally empty
+resizeGrid(); // I don't know why I have to put this a second time, but for some reason it only works if it's there twice
